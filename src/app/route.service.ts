e214448e6c0621b0e639f07f19b51a5d8660970a
daniiltserin <daniilborowkow@yandex.ru>
2017-06-26 18:02:40 +0600

    routes setup

--- src/app/route.service.ts
+++ src/app/route.service.ts
@@ -1,10 +1,27 @@
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { CouponsComponent } from './coupons/coupons.component';
+import { CouponComponent } from './coupon/coupon.component';
+import { NewsComponent } from './news/news.component';
+import { NewsSingleComponent } from './news-single/news-single.component';
+import { PromoComponent } from './promo/promo.component';
+import { FaqComponent } from './faq/faq.component';
+import { BuyerComponent } from './buyer/buyer.component';
+import { SellerComponent } from './seller/seller.component';
 
 /**
  * array of routes
  */
 const routeList: Array<Object> = [
-    { path: '' }
+    { path: '/', component: MainpageComponent },
+    { path: '/coupons', component: CouponsComponent },
+    { path: '/coupons/:id', component: CouponComponent },
+    { path: '/news', component: NewsComponent },
+    { path: '/news/:id', NewsSingleComponent },
+    { path: '/promo/:slug', PromoComponent },
+    { path: '/faq', component: FaqComponent },
+    { path: '/my', component: BuyerComponent },
+    { path: '/cabinet', component: SellerComponent }
 ];
 
 /**
