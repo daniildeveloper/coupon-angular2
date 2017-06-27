ab6ce797a5cde5ce00f838eb4f508f86b9ba48b6
daniiltserin <daniilborowkow@yandex.ru>
2017-06-27 10:09:55 +0600

    single coupon is top data binding

--- src/app/coupons-chunk/coupons-chunk.component.ts
+++ src/app/coupons-chunk/coupons-chunk.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceProvider } from '../api.service';
 
+/**
+ * all simple coupons are here
+ */
 @Component({
   selector: 'app-coupons-chunk',
   templateUrl: './coupons-chunk.component.html',
   styleUrls: ['./coupons-chunk.component.css']
 })
 export class CouponsChunkComponent implements OnInit {
-
-  constructor() { }
+  coupons;
+  
+  constructor(private _api: ApiServiceProvider) { }
 
   ngOnInit() {
+    this._api.getCoupons().subscribe(
+      data => {
+        this.coupons = data['items'];
+      }
+    )
+  }
+
+  isTop() {
+    let style = {
+      'border':  '5px solid #FEC52E'
+    };
+    
+
   }
 
 }
